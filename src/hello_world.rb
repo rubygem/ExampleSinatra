c5b2a2f82a0584474d182388614687fff7230cce
rubygem <gemma.cameron@gmail.com>
2013-07-17 11:16:00 +0100

    change the code so can test with some DI

--- src/hello_world.rb
+++ src/hello_world.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
-get '/' do
-	"Hello World#{params[:name]}"
+class HelloWorld < Sinatra::Base
+
+	get '/' do
+		"Hello World#{params[:name]}"
+	end
+
 end
--- tests/a-test.rb
+++ tests/a-test.rb
@@ -7,32 +7,15 @@ set :environment, :test
 class RackTests < Test::Unit::TestCase
 
   def test_it_says_hello_world
-    browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
+    browser = Rack::Test::Session.new(Rack::MockSession.new(HelloWorld.new))
     browser.get '/'
     assert browser.last_response.ok?
     assert_equal 'Hello World', browser.last_response.body
   end
 
   def test_it_says_hello_to_a_person
-    browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
+    browser = Rack::Test::Session.new(Rack::MockSession.new(HelloWorld.new))
     browser.get '/', :name => 'Simon'
     assert browser.last_response.body.include?('Simon')
   end
-end
-
-class HelloWorldTest < Test::Unit::TestCase
-	require 'capybara'
-	require 'capybara/dsl'
-
-	include Capybara::DSL
-	# Capybara.default_driver = :selenium # <-- use Selenium driver
-
-	def setup
-		Capybara.app = Sinatra::Application.new
-	end
-
-	def test_it_works
-		visit '/'
-		assert page.has_content?('Hello World')
-	end
 end
